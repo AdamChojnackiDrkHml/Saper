bc1ec8ac970309023e5d6fa05c7041d60a35d544
AdamChojnackiDrkHml <72651971+AdamChojnackiDrkHml@users.noreply.github.com>
2022-02-18 00:57:42 +0100

    Add basic io handling

--- app.go
+++ app.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"Saper/game"
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -9,5 +13,28 @@ func main() {
 	controlChannel := make(chan bool, 1)
 	go game.Timer(controlChannel)
 
-	time.Sleep(10 * time.Second)
+	time.Sleep(0 * time.Second)
+	controlChannel <- true
+	board := game.CreateBoard(5, 5, 4)
+	board.PrintBoard()
+	board.PrintPlayerBoard()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+
+		txt, _, _ := reader.ReadLine()
+		cords := strings.Split(string(txt), " ")
+		x, err := strconv.Atoi(cords[0])
+		if err != nil {
+			break
+		}
+		y, err := strconv.Atoi(cords[1])
+		if err != nil {
+			break
+		}
+
+		board.CheckField(x, y)
+		board.PrintPlayerBoard()
+
+	}
 }
