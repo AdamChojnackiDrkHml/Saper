45ca26cebfd2d199af50bddc508f3ddeb5d83c5b
AdamChojnackiDrkHml <72651971+AdamChojnackiDrkHml@users.noreply.github.com>
2022-02-18 00:56:47 +0100

    Adjust fieldEnum.go

---
+++ game/fieldEnum.go
@@ -0,0 +1,16 @@
+package game
+
+type FieldType int
+
+const (
+	ONE   FieldType = 1
+	TWO             = 2
+	THREE           = 3
+	FOUR            = 4
+	FIVE            = 5
+	SIX             = 6
+	SEVEN           = 7
+	EIGHT           = 8
+	ZERO            = 0
+	BOMB            = 9
+)
